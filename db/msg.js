36b8dca8bd51073b001a68d93f6a2350eae8784d
David Zou <davidzsy20@gmail.com>
2018-05-10 01:16:53 -0400

    Update msg.js

--- db/msg.js
+++ db/msg.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema
 var msgSchema = new Schema( {
     myMessage: String, 
     botMessage: String
-})
+}) 
 var Message = mongoose.model('Message', msgSchema)
 module.exports = Message
